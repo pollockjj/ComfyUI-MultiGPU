22b6b6c95dd544b2d127693bd14cb118704dd639
John Pollock <pollockjj@gmail.com>
2025-03-27 07:03:01 -0500

    dynamic one-pass L1 and L2 caching coded and working.

--- ggml_weight_utils.py
+++ ggml_weight_utils.py
@@ -5,14 +5,11 @@ import logging
 import comfy.model_management as mm
 import comfy.model_management
 
-
-
-
 GGMLTensor = importlib.import_module('custom_nodes.ComfyUI-GGUF.ops').GGMLTensor
 dequantize_tensor = importlib.import_module('custom_nodes.ComfyUI-GGUF.dequant').dequantize_tensor
 
 from . import SMALL_TENSOR_THRESHOLD
-COMPUTE_CACHE_LIMIT  = 0.80
+COMPUTE_CACHE_LIMIT  = 0.66
 TENSORATOR_CACHE_LIMIT  = 0.95
 TENSORATOR_BUFFER_SIZE_MB  = 1024
 DISABLE_DEQUANTIZED_RING_BUFFER = True
@@ -36,12 +33,12 @@ compute_target_cache = mm.get_total_memory(compute_device) * COMPUTE_CACHE_LIMIT
 tensorator_target_cache = mm.get_total_memory(tensorator_device) * TENSORATOR_CACHE_LIMIT / (1024 * 1024)
 compute_vram_used = mm.get_total_memory(compute_device)/ (1024 * 1024) - mm.get_free_memory(compute_device) / (1024 * 1024)
 tensorator_vram_used = mm.get_total_memory(tensorator_device)/ (1024 * 1024) - mm.get_free_memory(tensorator_device) / (1024 * 1024)
-print(f"compute device size = {mm.get_total_memory(compute_device)/  (1024 * 1024)} | tensorator device size = {mm.get_total_memory(tensorator_device)/  (1024 * 1024)} | compute_target_cache: {compute_target_cache:.2f}MB | tensorator_target_cache: {tensorator_target_cache:.2f}MB | compute_current_vram: {compute_vram_used:.2f}MB | tensorator_current_vram: {tensorator_vram_used:.2f}MB") 
 
 if compute_device != tensorator_device:
     solo_gpu = False
 else:
     solo_gpu = True
+    tensorator_target_cache = 0
 
 tensor_inference_order = 0
 
@@ -109,10 +106,8 @@ def cast_bias_weight_patched(s, input=None, dtype=None, device=None, bias_dtype=
             tensor_inference_order += 1
             distorch_load_map[source_tensor_hash_bias]['initialized'] = True
             bias = s.get_weight(s.bias.to(device), dtype, distorch_load_map[source_tensor_hash_bias]['tensor_inference_order'], distorch_load_map[source_tensor_hash_bias]['name'], distorch_load_map[source_tensor_hash_bias]['source_tensor'], distorch_load_map[source_tensor_hash_bias]['distorch_device'], source_tensor_hash_bias)
-            #logging.info(f"  Bias hash found: 0x{source_tensor_hash_bias:x} | tensor_inference_order: {distorch_load_map[source_tensor_hash_bias]['tensor_inference_order']:3d} | Size: {distorch_load_map[source_tensor_hash_bias]['tensor_size']:3.2f}MB | To compute: {device} | name: {distorch_load_map[source_tensor_hash_bias]['name']}")          
         elif source_tensor_hash_bias in distorch_load_map and distorch_load_map[source_tensor_hash_bias]['initialized'] is True:
             bias = s.get_weight(s.bias.to(device), dtype, distorch_load_map[source_tensor_hash_bias]['tensor_inference_order'], distorch_load_map[source_tensor_hash_bias]['name'], distorch_load_map[source_tensor_hash_bias]['source_tensor'], distorch_load_map[source_tensor_hash_bias]['distorch_device'], source_tensor_hash_bias)
-            #logging.info(f"  Bias hash hit: 0x{source_tensor_hash_bias:x} | tensor_inference_order: {distorch_load_map[source_tensor_hash_bias]['tensor_inference_order']:3d} | Size: {distorch_load_map[source_tensor_hash_bias]['tensor_size']:3.2f}MB | To compute: {device} | name: {distorch_load_map[source_tensor_hash_bias]['name']}")
         else:
             bias = s.get_weight(s.bias.to(device), dtype)
             logging.info(f"  Bias hash not found: 0x{source_tensor_hash_bias:x}")
@@ -126,15 +121,11 @@ def cast_bias_weight_patched(s, input=None, dtype=None, device=None, bias_dtype=
         tensor_inference_order += 1
         distorch_load_map[source_tensor_hash_weight]['initialized'] = True
         weight = s.get_weight(s.weight.to(device), dtype, distorch_load_map[source_tensor_hash_weight]['tensor_inference_order'], distorch_load_map[source_tensor_hash_weight]['name'], distorch_load_map[source_tensor_hash_weight]['source_tensor'], distorch_load_map[source_tensor_hash_weight]['distorch_device'], source_tensor_hash_weight)
-        #logging.info(f"Weight hash found: 0x{source_tensor_hash_weight:x} | tensor_inference_order: {distorch_load_map[source_tensor_hash_weight]['tensor_inference_order']:3d} | Size: {distorch_load_map[source_tensor_hash_weight]['tensor_size']:3.2f}MB | To compute: {device} | name: {distorch_load_map[source_tensor_hash_weight]['name']}" )     
     elif source_tensor_hash_weight in distorch_load_map and distorch_load_map[source_tensor_hash_weight]['initialized'] is True:
         weight = s.get_weight(s.weight.to(device), dtype, distorch_load_map[source_tensor_hash_weight]['tensor_inference_order'], distorch_load_map[source_tensor_hash_weight]['name'], distorch_load_map[source_tensor_hash_weight]['source_tensor'], distorch_load_map[source_tensor_hash_weight]['distorch_device'], source_tensor_hash_weight)
-        #logging.info(f"Weight hash hit: 0x{source_tensor_hash_weight:x} | tensor_inference_order: {distorch_load_map[source_tensor_hash_weight]['tensor_inference_order']:3d} | Size: {distorch_load_map[source_tensor_hash_weight]['tensor_size']:3.2f}MB | To compute: {device} | name: {distorch_load_map[source_tensor_hash_weight]['name']}" )
     else:
         weight = s.get_weight(s.weight.to(device), dtype)
-        #logging.info(f"  Weight hash not found: 0x{source_tensor_hash_weight:x}")
- 
-
+    
     weight = comfy.ops.cast_to(weight, dtype, device, non_blocking=non_blocking, copy=False)
     
     return weight, bias
@@ -148,26 +139,26 @@ def get_weight(ggml_tensor, dtype, dequant_dtype=None, patch_dtype=None, tensor_
 
         if source_tensor_hash in cached_tensor_map:
             if cached_tensor_map[source_tensor_hash]['cache_level'] == "level1":
-                compute_vram_used = mm.get_total_memory(compute_device)/ (1024 * 1024) - mm.get_free_memory(compute_device) / (1024 * 1024)
+                #compute_vram_used = mm.get_total_memory(compute_device)/ (1024 * 1024) - mm.get_free_memory(compute_device) / (1024 * 1024)
                 #print(f"compute_vram_used: {compute_vram_used:.2f}MB | compute_target_cache {compute_target_cache:.2f}MB")
                 #if compute_vram_used > compute_target_cache:
                 #    current_tensor = cached_tensor_map[source_tensor_hash]['cached_final_tensor']
                 #    lowest_priority = max(cached_tensor_map, key=lambda x: cached_tensor_map[x]['cache_priority'])
-                #    lowest_priority_index = next((index for (index, d) in enumerate(level_one_tensors) if d == cached_tensor_map[lowest_priority]['cached_final_tensor']), None)
-                #    level_one_tensors.pop(lowest_priority_index)
+                #    eviction_candidate_index = next((index for (index, d) in enumerate(level_one_tensors) if d == cached_tensor_map[lowest_priority]['cached_final_tensor']), None)
+                #    level_one_tensors.pop(eviction_candidate_index)
                 #    cached_tensor_map[lowest_priority]['cache_level'] = "prioritized"
                 #    cached_tensor_map[lowest_priority]['cached_final_tensor'] = None
                 #    return current_tensor
                 return cached_tensor_map[source_tensor_hash]['cached_final_tensor']
             elif cached_tensor_map[source_tensor_hash]['cache_level'] == "level2":
-                tensorator_vram_used = mm.get_total_memory(tensorator_device)/ (1024 * 1024) - mm.get_free_memory(tensorator_device) / (1024 * 1024)
+                #tensorator_vram_used = mm.get_total_memory(tensorator_device)/ (1024 * 1024) - mm.get_free_memory(tensorator_device) / (1024 * 1024)
                 with torch.cuda.stream(tensorator_stream):
                     level_two_tensor = cached_tensor_map[source_tensor_hash]['cached_final_tensor']
                     level_two_tensor.to(compute_device, non_blocking=True)
                     #if tensorator_vram_used > tensorator_target_cache:
                     #    lowest_priority = max(cached_tensor_map, key=lambda x: cached_tensor_map[x]['cache_priority'])
-                    #    lowest_priority_index = next((index for (index, d) in enumerate(level_two_tensors) if d == cached_tensor_map[lowest_priority]['cached_final_tensor']), None)
-                    #    level_two_tensors.pop(lowest_priority_index)
+                    #    eviction_candidate_index = next((index for (index, d) in enumerate(level_two_tensors) if d == cached_tensor_map[lowest_priority]['cached_final_tensor']), None)
+                    #    level_two_tensors.pop(eviction_candidate_index)
                     #    cached_tensor_map[lowest_priority]['cache_level'] = "prioritized"
                     #    cached_tensor_map[lowest_priority]['cached_final_tensor'] = None
                     tensorator_event.record(tensorator_stream)
@@ -200,43 +191,71 @@ def get_weight(ggml_tensor, dtype, dequant_dtype=None, patch_dtype=None, tensor_
             if source_tensor_hash in cached_tensor_map and cached_tensor_map[source_tensor_hash]['cache_level'] == "prioritized":
                 compute_vram_used = mm.get_total_memory(compute_device)/ (1024 * 1024) - mm.get_free_memory(compute_device) / (1024 * 1024)
                 tensorator_vram_used = mm.get_total_memory(tensorator_device)/ (1024 * 1024) - mm.get_free_memory(tensorator_device) / (1024 * 1024)
-                lowest_priority = max(cached_tensor_map, key=lambda x: cached_tensor_map[x]['cache_priority'])
 
-                
-                print(f"compute_vram_used: {compute_vram_used:.2f}MB | tensorator_vram_used: {tensorator_vram_used:.2f}MB | compute_target_cache {compute_target_cache:.2f}MB | tensorator_target_cache {tensorator_target_cache:.2f}MB")
+                #print(f"compute_vram_used: {compute_vram_used:.2f}MB | tensorator_vram_used: {tensorator_vram_used:.2f}MB | compute_target_cache {compute_target_cache:.2f}MB | tensorator_target_cache {tensorator_target_cache:.2f}MB")
                 if compute_vram_used <= compute_target_cache or cached_tensor_map[source_tensor_hash]['cache_priority'] == -1:
                     level_one_tensor = tensorator_tensor.clone().to(compute_device, non_blocking=True)
+                    level_one_tensor.original_hash = source_tensor_hash
                     level_one_tensors.append(level_one_tensor)
                     cached_tensor_map[source_tensor_hash]['cached_final_tensor'] = level_one_tensor
                     cached_tensor_map[source_tensor_hash]['cache_level'] = "level1"
-                    print(f"Caching Level 1 Tensor: 0x{source_tensor_hash:x} | tensor_inference_order: {cached_tensor_map[source_tensor_hash]['tensor_inference_order']:3d} | Size: {cached_tensor_map[source_tensor_hash]['tensor_size']:8.2f}MB | to    compute_device: {compute_device}")
+                    #print(f"Caching Level 1 Tensor: 0x{source_tensor_hash:x} | tensor_inference_order: {cached_tensor_map[source_tensor_hash]['tensor_inference_order']:3d} | Size: {cached_tensor_map[source_tensor_hash]['tensor_size']:8.2f}MB | to    compute_device: {compute_device}")
                     tensorator_event.record(tensorator_stream)
                     torch.cuda.current_stream().wait_event(tensorator_event)
                     return level_one_tensor
-                elif compute_vram_used > compute_target_cache and lowest_priority>cached_tensor_map[source_tensor_hash]['cache_priority']:
-                    lowest_priority_index = next((index for (index, d) in enumerate(level_one_tensors) if d == cached_tensor_map[lowest_priority]['cached_final_tensor']), None)
-                    level_one_tensors.pop(lowest_priority_index)
-                    cached_tensor_map[lowest_priority]['cache_level'] = "prioritized"
-                    cached_tensor_map[lowest_priority]['cached_final_tensor'] = None
-                    print(f"Evicting Level 1 Tensor: 0x{lowest_priority:x} | tensor_inference_order: {cached_tensor_map[lowest_priority]['tensor_inference_order']:3d} | Size: {cached_tensor_map[lowest_priority]['tensor_size']:8.2f}MB | from compute_device: {compute_device}")
-                    level_one_tensor = tensorator_tensor.clone().to(compute_device, non_blocking=True)
-                    level_one_tensors.append(level_one_tensor)
-                    cached_tensor_map[source_tensor_hash]['cached_final_tensor'] = level_one_tensor
-                    cached_tensor_map[source_tensor_hash]['cache_level'] = "level1"
-                    print(f"Caching Level 1 Tensor: 0x{source_tensor_hash:x} | tensor_inference_order: {cached_tensor_map[source_tensor_hash]['tensor_inference_order']:3d} | Size: {cached_tensor_map[source_tensor_hash]['tensor_size']:8.2f}MB | to    compute_device: {compute_device}")
-                    tensorator_event.record(tensorator_stream)
-                    torch.cuda.current_stream().wait_event(tensorator_event)
-                    return level_one_tensor
-                    
-                elif tensorator_vram_used <= tensorator_target_cache and solo_gpu == False:
-                    level_two_tensor = tensorator_tensor.clone().to(tensorator_device, non_blocking=True)
-                    level_two_tensors.append(level_two_tensor)
-                    cached_tensor_map[source_tensor_hash]['cached_final_tensor'] = level_two_tensor
-                    cached_tensor_map[source_tensor_hash]['cache_level'] = "level2"
-                    print(f"Caching Level 2 Tensor: 0x{source_tensor_hash:x} | tensor_inference_order: {cached_tensor_map[source_tensor_hash]['tensor_inference_order']:3d} | Size: {cached_tensor_map[source_tensor_hash]['tensor_size']:8.2f}MB | to tensorator_device: {tensorator_device}")
-                    tensorator_event.record(tensorator_stream)
-                    torch.cuda.current_stream().wait_event(tensorator_event)
-                    return level_two_tensor
+                elif compute_vram_used > compute_target_cache:
+                    tensor_to_evict = None
+                    max_level1_priority_value = -1
+
+                    for cached_tensor in level_one_tensors:
+                        if cached_tensor_map[cached_tensor.original_hash]['cache_priority'] > max_level1_priority_value:
+                            max_level1_priority_value = cached_tensor_map[cached_tensor.original_hash]['cache_priority']
+                            tensor_to_evict = cached_tensor
+
+                    if max_level1_priority_value > cached_tensor_map[source_tensor_hash]['cache_priority']:
+                        eviction_candidate_index = next(index for index, d in enumerate(level_one_tensors) if d is tensor_to_evict)
+                        evicted_tensor_ref = level_one_tensors.pop(eviction_candidate_index)
+
+                        if tensorator_vram_used < tensorator_target_cache:
+                            xfered_to_level2 = evicted_tensor_ref.to(tensorator_device, non_blocking=True)
+                            level_two_tensors.append(xfered_to_level2)
+                            cached_tensor_map[tensor_to_evict.original_hash]['cache_level'] = "level2"
+                            cached_tensor_map[tensor_to_evict.original_hash]['cached_final_tensor'] = xfered_to_level2
+                            #print(f"Transferring Level 1 tensor to Level 2: 0x{tensor_to_evict.original_hash:x} | Size: {cached_tensor_map[tensor_to_evict.original_hash]['tensor_size']:8.2f}MB | to {tensorator_device}")
+                        else:
+                            cached_tensor_map[tensor_to_evict.original_hash]['cache_level'] = "prioritized"
+                            cached_tensor_map[tensor_to_evict.original_hash]['cached_final_tensor'] = None
+                            #print(f"Evicting Level 1 Tensor: 0x{tensor_to_evict.original_hash:x} | tensor_inference_order: {cached_tensor_map[tensor_to_evict.original_hash]['tensor_inference_order']:3d} | Size: {cached_tensor_map[tensor_to_evict.original_hash]['tensor_size']:8.2f}MB | from compute_device: {compute_device}")
+
+                        level_one_tensor = tensorator_tensor.clone().to(compute_device, non_blocking=True)
+                        level_one_tensor.original_hash = source_tensor_hash
+                        level_one_tensors.append(level_one_tensor)
+                        cached_tensor_map[source_tensor_hash]['cached_final_tensor'] = level_one_tensor
+                        cached_tensor_map[source_tensor_hash]['cache_level'] = "level1"
+                        #print(f"Caching Level 1 Tensor: 0x{source_tensor_hash:x} | tensor_inference_order: {cached_tensor_map[source_tensor_hash]['tensor_inference_order']:3d} | Size: {cached_tensor_map[source_tensor_hash]['tensor_size']:8.2f}MB | to    compute_device: {compute_device}")
+
+                        tensorator_event.record(tensorator_stream)
+                        torch.cuda.current_stream().wait_event(tensorator_event)
+                        return level_one_tensor
+                    else:
+                        if tensorator_vram_used < tensorator_target_cache:
+                            level_two_tensor = tensorator_tensor.clone().to(tensorator_device, non_blocking=True)
+                            level_two_tensor.original_hash = source_tensor_hash
+                            level_two_tensors.append(level_two_tensor)
+                            cached_tensor_map[source_tensor_hash]['cached_final_tensor'] = level_two_tensor
+                            cached_tensor_map[source_tensor_hash]['cache_level'] = "level2"
+                            #print(f"Caching Level 2 Tensor: 0x{source_tensor_hash:x} | Size: {cached_tensor_map[source_tensor_hash]['tensor_size']:8.2f}MB | to {tensorator_device}")
+                            tensorator_event.record(tensorator_stream)
+                            tensor_on_compute = level_two_tensor.to(compute_device, non_blocking=True)
+                            torch.cuda.current_stream().wait_event(tensorator_event)
+                            compute_event.record(torch.cuda.current_stream())
+                            compute_event.wait()
+                            return tensor_on_compute
+                        else:
+                            tensor_on_compute = tensorator_tensor.to(device=compute_device, non_blocking=True)
+                            tensorator_event.record(tensorator_stream)
+                            torch.cuda.current_stream().wait_event(tensorator_event)
+                            return tensor_on_compute
 
             tensorator_tensor = tensorator_tensor.to(device=compute_device, non_blocking=True)
             tensorator_event.record(tensorator_stream)
@@ -279,6 +298,5 @@ def get_weight(ggml_tensor, dtype, dequant_dtype=None, patch_dtype=None, tensor_
                 cached_tensor_map[source_tensor_hash]['cache_priority'] = -1
                 #print(f"Initializing Cache Entry: hash=0x{source_tensor_hash:x} | tensor_inference_order={tensor_inference_order:3d} | patches={len(patch_list)} | device={distorch_device} | size={cached_tensor_map[source_tensor_hash]['tensor_size']:.2f}MB | name={name}")
 
-  
     torch.cuda.current_stream().wait_event(tensorator_event)
     return tensorator_tensor
